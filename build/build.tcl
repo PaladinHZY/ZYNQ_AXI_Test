6bd60fffb30f19017470eb37a7c36126e4578fb3
Zhenyu Hu <zhenyu@ainstein.ai>
2019-04-01 14:28:29 -0500

    Modify two tcl files

--- build/build.tcl
+++ build/build.tcl
@@ -1,4 +1,4 @@
 open_project project_1/project_1.xpr
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1 
-write_hwdef -force ../sdk/ZYBO_AXI_Test.hdf
+write_hwdef -force ../sdk/design_1_wrapper.hdf
--- build/sdk.tcl
+++ build/sdk.tcl
@@ -1,7 +1,8 @@
 sdk setws ../sdk/
-sdk createhw -name AXI_Datalink_Test_hw_platform -hwspec ../sdk/ZYBO_AXI_Test.hdf
+sdk createhw -name AXI_Datalink_Test_hw_platform -hwspec ../sdk/design_1_wrapper.hdf
 createbsp -name AXI_Datalink_Test_bsp -hwproject AXI_Datalink_Test_hw_platform -proc ps7_cortexa9_0 -os standalone
 setlib -bsp AXI_Datalink_Test_bsp -lib xilffs
 regenbsp -bsp AXI_Datalink_Test_bsp
 createapp -name AXI_Datalink_Test -bsp AXI_Datalink_Test_bsp -app {Empty Application} -hwproject AXI_Datalink_Test_hw_platform -proc ps7_cortexa9_0
+#createapp -name AXI_Datalink_Test -bsp AXI_Datalink_Test_bsp -hwproject AXI_Datalink_Test_hw_platform -proc ps7_cortexa9_0
 importprojects ../sdk/AXI_Datalink_Test
